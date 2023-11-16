f90c2ec91e8b81d9b081c6192e2d305ddec84929
LeandroLicata <leandrolicata1@gmail.com>
2023-11-16 08:51:31 -0300

    pre-reset

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        {/* <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap"
+        /> */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500&family=Press+Start+2P&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "graybg": "#5a5b60",
+        "pink-neon": "#FF1493",
+        "blue-neon": "#00A7E1",
+        "bluebg": "#040a24",
+        "pink": "#c92ec7",
+        "blue": "#2440b7",
+        "light-blue": "#0dd6fa"
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
