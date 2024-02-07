38a7398246f55340bd5342d9a846c1797d90a666
Leandro Licata <leandrolicata1@gmail.com>
2024-02-07 13:24:05 -0300

    about animation

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -6,14 +6,14 @@ const AboutSection = () => {
       <div className="about-section about-content-card text-light-blue p-5 overflow-auto">
         <div className="text-sm">
           <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.5 }} className="text-lg text-blue-neon">Acerca de mí</motion.h1>
-          <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.6 }} className="pt-5">
+          <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.7 }} className="pt-5">
             ¡Hola! Soy <span className="text-pink">Leandro Licata</span>, un
             apasionado desarrollador web con una trayectoria única. Inicialmente
             formado como técnico electricista con orientación en electrónica y
             técnico en armado y reparación de PC, decidí dar un giro en mi vida
             en 2022 para seguir mi verdadera pasión: la programación.
           </motion.p>
-          <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.7 }} className="pt-5">
+          <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.9 }} className="pt-5">
             Me gradué como{" "}
             <span className="text-pink">Full Stack Web Developer</span> en el
             bootcamp SoyHenry, donde adquirí sólidos conocimientos en
@@ -30,7 +30,7 @@ const AboutSection = () => {
             metodologías ágiles Scrum fue una experiencia valiosa que reforzó mi
             enfoque colaborativo y orientado a resultados.
           </p> */}
-          <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.8 }} className="pt-5">
+          <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 1.1 }} className="pt-5">
             Actualmente, estoy en la búsqueda de nuevas oportunidades laborales
             en el mundo de la programación. Estoy convencido de que mi
             experiencia diversa y mis habilidades técnicas pueden hacer
