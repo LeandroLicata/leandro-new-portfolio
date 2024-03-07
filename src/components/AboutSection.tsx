2aa5a37afd0dc4f5a513dfe8a8214d00a53d0b70
Leandro Licata <leandrolicata1@gmail.com>
2024-03-07 17:00:38 -0300

    about actualizado

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const AboutSection = () => {
   return (
     <section>
-      <div className="about-section about-content-card text-light-blue p-5 h-[75%] md:h-[85%]">
+      <div className="about-section about-content-card text-light-blue p-5">
         <div className="text-xs md:text-sm overflow-auto h-[100%]">
           <motion.h1
             initial={{ opacity: 0, scale: 0.5 }}
@@ -19,8 +19,10 @@ const AboutSection = () => {
             transition={{ duration: 0.5, delay: 0.7 }}
             className="pt-5"
           >
-            ¡Hola! Soy <span className="text-pink">Leandro Licata</span>, un
-            apasionado desarrollador web.
+            ¡Soy <span className="text-pink">Leandro</span>! Me formé como
+            Técnico Electricista y Técnico en Armado y Reparación de PC y, tras
+            años ejerciendo, me pasé al mundo de la Programación. Desde entonces
+            disfruto creando experiencias web para los usuarios.
           </motion.p>
           <motion.p
             initial={{ opacity: 0, scale: 0.5 }}
@@ -28,13 +30,19 @@ const AboutSection = () => {
             transition={{ duration: 0.5, delay: 0.9 }}
             className="pt-5"
           >
-            Me gradué como{" "}
-            <span className="text-pink">Full Stack Web Developer</span> en el
-            bootcamp SoyHenry, donde adquirí sólidos conocimientos en
-            JavaScript, HTML, CSS, Node, React, Redux y SQL, entre otras
-            tecnologías. Mi viaje no se detuvo allí; continué mi formación,
-            explorando herramientas como Bootstrap, Next, MongoDB y Cloudinary
-            para ampliar mi conjunto de habilidades.
+            Entre mis éxitos destaco que, durante mi tiempo en{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.timbring.com/"
+              className="text-pink"
+            >
+              Timbring
+            </a>
+            , haber migrado exitosamente la autenticación a NextAuth y haber
+            trabajado en la optimización del formulario de registro. Esto
+            permitió que cientos de usuarios se registraran e iniciaran sesión
+            de manera más rápida y eficiente.
           </motion.p>
           <motion.p
             initial={{ opacity: 0, scale: 0.5 }}
@@ -42,23 +50,17 @@ const AboutSection = () => {
             transition={{ duration: 0.5, delay: 1.1 }}
             className="pt-5"
           >
-            Mi experiencia se ha enriquecido aún más trabajando en proyectos del
-            mundo real, donde perfeccioné mis habilidades y adquirí nuevas
-            competencias en TypeScript, TailwindCSS y Nodemailer. Trabajar en
-            equipo con metodologías ágiles Scrum fue una experiencia valiosa que
-            reforzó mi enfoque colaborativo y orientado a resultados.
-          </motion.p>
-          <motion.p
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5, delay: 1.3 }}
-            className="pt-5"
-          >
-            Actualmente, estoy en la búsqueda de nuevas oportunidades laborales
-            en el mundo de la programación. Estoy convencido de que mi
-            experiencia diversa y mis habilidades técnicas pueden hacer
-            contribuciones significativas a cualquier proyecto en el que
-            participe.
+            Cuento con experiencia en el desarrollo de aplicaciones web, y entre
+            mis proyectos destacados se encuentra Gamepedia. Este proyecto me
+            permitió combinar dos de mis pasiones: la programación y los
+            videojuegos. En <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://gamepedia-gaming.vercel.app/"
+              className="text-pink"
+            >Gamepedia</a>, ayudé a los usuarios a obtener
+            información sobre cualquier videojuego y a ver capturas de pantalla
+            de los mismos. ¡Te invito a echarle un vistazo!
           </motion.p>
         </div>
       </div>
